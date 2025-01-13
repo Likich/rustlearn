51c8d0cf295795c8d879a0c2324b50a62d2025ed
Likich <anyur.pa@gmail.com>
2025-01-13 05:09:50 -0800

    variables

---
+++ variables/target/debug/deps/variables-5bbaae70a4d30ed4.d
@@ -0,0 +1,5 @@
+/Users/angelinaparfenova/rustlearn/rustlearn/variables/target/debug/deps/variables-5bbaae70a4d30ed4: src/main.rs
+
+/Users/angelinaparfenova/rustlearn/rustlearn/variables/target/debug/deps/variables-5bbaae70a4d30ed4.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/angelinaparfenova/rustlearn/rustlearn/variables/target/debug/variables: /Users/angelinaparfenova/rustlearn/rustlearn/variables/src/main.rs
